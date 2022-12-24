8f5212ee6b05db2bb01b72aea8df2b6565aa5dd4
NandoApri26 <apriadno.pratamaa@gmail.com>
2022-12-24 12:09:37 +0700

    create aspirasi

---
+++ resources/views/aspirasi/create.blade.php
@@ -0,0 +1,41 @@
+@extends('templates.main')
+@section('title', 'Create Aspirasi')
+
+@section('content')
+<div class="mt-20 text-center font-poppins text-xl font-bold text-gray-500">
+    <h1>Create Aspirasi</h1>
+</div>
+<div class="bg-gray-200 mt-10 p-5">
+
+    <form method="POST" action="{{url('/store')}}" enctype="multipart/form-data">
+        @csrf
+        <div class="mb-6 ">
+            <label for="nama" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
+            <input type="text" id="nama" name="nama" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[450px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Insert your Nama" required>
+        </div>
+        <div class="mb-6 ">
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+            <input type="email" id="email" name="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[450px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Insert your email" required>
+        </div>
+        
+        <div class="mb-6">
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="foto">Foto</label>
+            <input class="block w-[450px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="user_avatar_help" id="user_avatar" type="file" name="foto" id="foto">
+        </div>
+
+        <div class="mb-6">
+            <label for="cerita" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">cerita</label>
+            <textarea id="cerita" name="cerita" rows="4" class="block p-2.5 w-[450px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Insert your cerita" required></textarea>
+        </div>
+
+        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create</button>
+
+        <a href="{{ url ('/aspirasi') }}" class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                Back
+            </span>
+        </a>
+    </form>
+
+</div>
+@endsection
