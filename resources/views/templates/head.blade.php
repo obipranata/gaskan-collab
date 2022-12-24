1c445c310775962fc6c03b93d06d3383de588c8d
NandoApri26 <apriadno.pratamaa@gmail.com>
2022-12-24 14:01:46 +0700

    styling

--- resources/views/templates/head.blade.php
+++ resources/views/templates/head.blade.php
@@ -4,5 +4,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
     <link href="{{asset('/css/app.css')}}" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/3d647cac5e.js" crossorigin="anonymous"></script>
+  
 </head>
