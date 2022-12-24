5ce7f3c3b48201489febabb1b23b15e5f291dee2
NandoApri26 <apriadno.pratamaa@gmail.com>
2022-12-24 15:14:17 +0700

    design navbar

--- resources/views/templates/navbar.blade.php
+++ resources/views/templates/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
     <div class="container flex flex-wrap items-center justify-between mx-auto">
         <a href="{{ ('') }}" class="flex items-center">
-            <h1 class="font-bold text-xl">ASPIRASI</h1>
+            <h1 class="font-bold text-xl text-gray-400">Aspirasi<span class="font-bold text-orange-300">Rakyat</span></h1>
         </a>
         @if (session()->has('logged'))
             <div class="flex md:order-2">
@@ -25,20 +25,20 @@
             </div>
         @endif
         <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-            <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul class="flex flex-col p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
                 <li>
-                    <a href="{{ url ('/') }}" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent {{Request::segment(1) == '' ? 'md:text-blue-700' : 'text-gray-700'}} md:p-0 dark:text-white" aria-current="page">Home</a>
+                    <a href="{{ url ('/') }}" class="block py-2 pl-3 pr-4 text-gray-600 bg-blue-700 rounded md:bg-transparent {{Request::segment(1) == '' ? 'md:text-gray-700' : 'text-blue-700'}} md:p-0 dark:text-white" aria-current="page">Home</a>
                 </li>
                 @if (session()->has('logged'))
                     <li>
-                        <a href="{{ url ('/aspirasi') }}" class="block py-2 pl-3 pr-4 {{Request::segment(1) == 'aspirasi' ? 'md:text-blue-700' : 'text-gray-700'}} rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Aspirasi</a>
+                        <a href="{{ url ('/aspirasi') }}" class="block py-2 pl-3 pr-4 {{Request::segment(1) == 'aspirasi' ? 'md:text-gray-700' : 'text-blue-700'}} rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Aspirasi</a>
                     </li>
                     <li>
-                        <a href="{{ url ('/list-admin') }}" class="block py-2 pl-3 pr-4 {{Request::segment(1) == 'list-admin' ? 'md:text-blue-700' : 'text-gray-700'}} rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Admin</a>
+                        <a href="{{ url ('/list-admin') }}" class="block py-2 pl-3 pr-4 {{Request::segment(1) == 'list-admin' ? 'md:text-gray-700' : 'text-blue-700'}} rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Admin</a>
                     </li>
                     @else
                     <li>
-                        <a href="{{ url ('/aspirasi/create') }}" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Form Aspirasi</a>
+                        <a href="{{ url ('/aspirasi/create') }}" class="block pygrayl-3 pr-4 text-blue-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Form Aspirasi</a>
                     </li>
                 @endif
             </ul>
