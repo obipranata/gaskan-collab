a19e86084276b78cc268be48befd3f2c49c13649
HanifAkmaludin <hanif.akmaludin12@gmail.com>
2022-12-24 15:29:19 +0700

    menambahkan tombol back di login

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -15,37 +15,32 @@
                 <p class="font-bold msg-error cursor-pointer">X</p>
             </div>
             @endif
-            <form action="{{route('login')}}" method="POST" class="flex flex-col items-center gap-4">
+            <form action="{{route('login')}}" method="POST" class="flex flex-col gap-4">
             @csrf
-            {{-- @foreach($errors->all() as $error)
-                {{ $error }}
-            @endforeach --}}
-                <div class="w-full flex flex-col gap-1">
-                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Email</label>
-                    <input type="email" id="email" name="email"
-                        class="border border-[#EEE6C3] text-[12px] rounded-lg shadow-inner w-full"
-                        placeholder="Please insert your email" required />
-                </div>
-                <div class="w-full flex flex-col gap-1">
-                    <label for="password" class="block mb-2  text-sm font-medium text-gray-900">Password</label>
-                    <input type="password" id="password" name="password"
-                        class="border border-[#EEE6C3] w-full text-[12px] rounded-lg shadow-inner "
-                        placeholder="Please insert your password" required />
-                </div>
-                {{-- <div class="flex items-start mb-6">
-                    <div class="flex items-center h-5">
-                        <input id="remember" type="checkbox" value=""
-                            class="w-4 h-4 ml-6 md:ml-10 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-                            required />
+                <div class="flex flex-col gap-4">
+                    <div class="w-full flex flex-col gap-1">
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Email</label>
+                        <input type="email" id="email" name="email"
+                            class="border border-[#EEE6C3] text-[12px] rounded-lg shadow-inner w-full"
+                            placeholder="Please insert your email" required />
                     </div>
-                    <label for="remember" class="ml-2 text-sm font-medium text-white dark:text-gray-300">Remember
-                        me</label>
-                </div> --}}
-                <div class="text-center">
+                    <div class="w-full flex flex-col gap-1">
+                        <label for="password" class="block mb-2  text-sm font-medium text-gray-900">Password</label>
+                        <input type="password" id="password" name="password"
+                            class="border border-[#EEE6C3] w-full text-[12px] rounded-lg shadow-inner "
+                            placeholder="Please insert your password" required />
+                    </div>
+                </div>
+                <div class="flex gap-4">
                     <button type="submit"
                         class="text-gray-500 bg-[#EEE6C3] hover:bg-[#FFE15D] px-8 py-2 font-poopins font-bold rounded-lg">
                         Login
                     </button>
+                    <a href="{{ url ('/') }}" class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+                        <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                            Back
+                        </span>
+                    </a>
                 </div>
             </form>
         </div>
