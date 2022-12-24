41c046d22263ae397a5cad91e471e8ee9dfaf8a5
obipranata <obiimanuelpranata@gmail.com>
2022-12-24 14:47:48 +0700

    (+) active link

--- resources/views/templates/navbar.blade.php
+++ resources/views/templates/navbar.blade.php
@@ -27,14 +27,14 @@
         <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
             <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
-                    <a href="{{ url ('/') }}" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
+                    <a href="{{ url ('/') }}" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent {{Request::segment(1) == '' ? 'md:text-blue-700' : 'text-gray-700'}} md:p-0 dark:text-white" aria-current="page">Home</a>
                 </li>
                 @if (session()->has('logged'))
                     <li>
-                        <a href="{{ url ('/aspirasi') }}" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Aspirasi</a>
+                        <a href="{{ url ('/aspirasi') }}" class="block py-2 pl-3 pr-4 {{Request::segment(1) == 'aspirasi' ? 'md:text-blue-700' : 'text-gray-700'}} rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Aspirasi</a>
                     </li>
                     <li>
-                        <a href="{{ url ('/list-admin') }}" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Admin</a>
+                        <a href="{{ url ('/list-admin') }}" class="block py-2 pl-3 pr-4 {{Request::segment(1) == 'list-admin' ? 'md:text-blue-700' : 'text-gray-700'}} rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Admin</a>
                     </li>
                     @else
                     <li>
